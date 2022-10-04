8c29cd4ed3bd7999775714d632ddf740dcad3774
danmiee <dan-mi@kakao.com>
2022-10-04 12:34:41 +0900

    setMessage 형태 변형

--- Test/src/bhhong/middletest_interface/Message.java
+++ Test/src/bhhong/middletest_interface/Message.java
@@ -3,7 +3,7 @@ package bhhong.middletest_interface;
 public interface Message {
 
 	public String getMessage();
-	public String setMessage();
+	public void setMessage(String gender);
 	public static void getConnection() {
 		System.out.println("network ok");
 	}
