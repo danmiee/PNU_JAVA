8e87a35c48574743edcbba3b54933aac005c5f37
danmiee <dan-mi@kakao.com>
2022-10-04 12:38:46 +0900

    반복문 범위오류 확인 및 수정

--- SelfStudy/src/baekjoon/step04/Array_1D/q01_MinMax_brbw_error.java
+++ SelfStudy/src/baekjoon/step04/Array_1D/q01_MinMax_brbw_error.java
@@ -11,31 +11,35 @@ public class q01_MinMax_brbw_error {
 
 	public static void main(String[] args) throws IOException {
 		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int n = Integer.parseInt(st.nextToken());
-		int N [] = new int [n];
-		
-		for(int i=1; i<=n; i++) {
-			N[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		int min = N[0];
-		int max = N[0];
-
-		for (int i = 0; i < n; i++) {
-			if (min >= N[i]) {
-				min = N[i];
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+			
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			int n = Integer.parseInt(st.nextToken());
+			int N [] = new int [n];
+			
+			for(int i=0; i<n; i++) {
+				N[i] = Integer.parseInt(st.nextToken());
 			}
-			if (max <= N[i]) {
-				max = N[i];
+			
+			int min = N[0];
+			int max = N[0];
+			
+			for (int i = 0; i < n; i++) {
+				if (min >= N[i]) {
+					min = N[i];
+				}
+				if (max <= N[i]) {
+					max = N[i];
+				}
 			}
+			bw.write(min + " " + max);
+			bw.close();			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
 		}
-		bw.write(min + " " + max);
-		bw.close();
 	}
 	
 }
