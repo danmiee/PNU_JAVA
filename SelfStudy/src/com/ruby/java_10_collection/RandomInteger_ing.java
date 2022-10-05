151ff2a3590760fdf9e3c2e76e681ed64fc5540e
danmiee <dan-mi@kakao.com>
2022-10-05 09:08:21 +0900

    컬렉션 문제 첫커밋

---
+++ SelfStudy/src/com/ruby/java_10_collection/RandomInteger_ing.java
@@ -0,0 +1,16 @@
+package com.ruby.java_10_collection;
+
+
+public class RandomInteger_ing {
+
+//	Scanner sc = new Scanner(System.in);
+//	
+//	int N [] = new int [100];
+//	
+//	for(int i=0; i<N.length; i++) {
+//		N[i]=sc.nextInt();
+//	}
+//	
+//	List<N> list = new ArrayList<>();
+	
+}
