79d20c58723af56eb8e2244cfae0a0da4b2da933
danmiee <dan-mi@kakao.com>
2022-10-19 20:03:35 +0900

    백준 단계별문제 문자열 - 1157 단어 공부 클래스활용

---
+++ SelfStudy/src/baekjoon/step06/string/q05_WordIncodingValue3.java
@@ -0,0 +1,63 @@
+package baekjoon.step06.string;
+
+import java.util.Scanner;
+
+class MostUsing {
+	
+	public int[] count(String str) {
+		
+		// 알파벳길이 배열 생성
+		int[] abc = new int[26];
+		
+		// str 돌면서 해당하는 index값 +1
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) >= 65 && str.charAt(i) <= 90) {
+				abc[str.charAt(i) - 65]++;
+			} else if (str.charAt(i) >= 97 && str.charAt(i) <= 122) {
+				abc[str.charAt(i) - 97]++;
+			} else {
+				System.out.println("영문자를 입력해주세요.");
+			}
+		}
+		return abc;
+	}
+	
+	public void print(int[] abc) {
+		
+		// max 값 찾기
+		int max = -1;
+		char res = '?';
+		
+		for (int i = 0; i < abc.length; i++) {
+			if (abc[i] > max) {
+				max = abc[i];
+				res = (char) (i + 65);
+			} else if (abc[i] == max) {
+				res = '?';
+				// break 넣어서 틀림 > break하면 알파벳 모두 확인이 안됨
+			}
+		}
+		System.out.println(res);
+	}
+	
+}
+
+public class q05_WordIncodingValue3 {
+	
+	public static void main(String[] args) {
+
+		// 아스키코드 활용하기
+		// 대문자: 65~90
+		// 소문자: 97~122
+
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		
+		MostUsing mu = new MostUsing();
+		
+		int[] arr = mu.count(str);
+		mu.print(arr);
+
+		sc.close();
+	}
+}
