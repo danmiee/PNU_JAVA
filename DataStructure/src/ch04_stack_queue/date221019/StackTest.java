bd00e9d2850a5ff027bddd1912350baa6320fcc8
danmiee <dan-mi@kakao.com>
2022-10-22 20:19:30 +0900

    출력내용 정리

--- DataStructure/src/ch04_stack_queue/date221019/StackTest.java
+++ DataStructure/src/ch04_stack_queue/date221019/StackTest.java
@@ -8,41 +8,39 @@ public class StackTest {
 		Scanner sc = new Scanner(System.in);
 		Stack<Integer> s = new Stack<>(3);
 
-		int menu = -1;
+		while (true) {
 
-		System.out.println();
-		System.out.printf("현재 데이터 갯수: %d / %d\n", s.size(), s.getCapacity());
-		System.out.println("원하는 작업 번호를 입력하세요");
-		System.out.print("(1)푸시  (2)팝  (3)피크  (4)덤프  (0)종료");
+			System.out.println();
+			System.out.printf("현재 데이터 갯수: %d / %d\n", s.size(), s.getCapacity());
+			System.out.print("(1)푸시  (2)팝  (3)피크  (4)덤프  (0)종료: ");
 
-		while ((menu = sc.nextInt()) != 0) {
-
-			Integer x;
+			int menu = sc.nextInt();
+			if (menu == 0)
+				break;
+			int x;
 
 			switch (menu) {
 
 			case 1: // 푸시
 				System.out.print("데이터 : ");
 				x = (Integer) sc.nextInt();
-				if(s.isFull())
+				if (s.isFull())
 					System.out.println("스택이 가득차있습니다.");
-				else {
-				s.push(x);
-				System.out.printf("현재 데이터 갯수: %d / %d\n", s.size(), s.getCapacity());
-				}
+				else
+					s.push(x);
 				break;
 
 			case 2: // 팝
-				if(s.isEmpty())
+				if (s.isEmpty())
 					System.out.println("스택이 비어있습니다.");
 				else {
-				x = s.pop();
-				System.out.println("팝한 데이터는 " + x + "입니다.");
+					x = s.pop();
+					System.out.println("팝한 데이터는 " + x + "입니다.");
 				}
 				break;
 
 			case 3: // 피크
-				if(s.isEmpty()) 
+				if (s.isEmpty())
 					System.out.println("스택이 비어있습니다.");
 				else {
 					x = s.peek();
