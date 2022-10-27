fa2760b421f07bc5239d8923c0e99dc353fb1480
danmiee <dan-mi@kakao.com>
2022-10-27 20:45:55 +0900

    백준 1152번 문제

--- SelfStudy/src/baekjoon/step06/string/q06_WordCount.java
+++ SelfStudy/src/baekjoon/step06/string/q06_WordCount.java
@@ -9,11 +9,20 @@ public class q06_WordCount {
 		String[] arr = str.split(" ");
 		int cnt = arr.length;
 		
+		// 아무것도 입력하지 않은 경우
+		if(arr.length == 0) {
+			System.out.println(0);
+			System.exit(0);			// 프로그램 강제종료
+		}
+			
 		if(arr[0].equals(""))
 			cnt--;
+		else
+			return cnt;
 		
-		if(arr[arr.length-1].equals(""))
-			cnt--;
+		// 마지막 스페이스는 컴파일러가 배열에 포함시키지 않으므로 불필요
+//		if(arr[arr.length-1].equals(""))
+//			cnt--;
 		
 		return cnt;
 	}
