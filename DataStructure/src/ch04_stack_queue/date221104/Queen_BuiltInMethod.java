e6f190a3bf93e80f68c3c5dcdb57ffa70968e798
danmiee <dan-mi@kakao.com>
2022-11-04 20:35:21 +0900

    SolveQueen 메소드 작성 / 해결완료

--- DataStructure/src/ch04_stack_queue/date221104/Queen_BuiltInMethod.java
+++ DataStructure/src/ch04_stack_queue/date221104/Queen_BuiltInMethod.java
@@ -7,88 +7,108 @@ public class Queen_BuiltInMethod {
 		int ix = 0, iy = 0;
 		Stack st = new Stack(10);
 		Point p = new Point(ix, iy);
-		d[ix][iy] = 1;
-		count++;
+		d[ix][iy] = 1; // 퀸 배치
+		count++; // 퀸 배치한 point 수
 		st.push(p);
+
+		// count가 8이 될때까지 반복
 		while (count < 8) {
-			ix++;
-			int cy = 0;
+			ix++; // 행변경
+			int cy = 0; // 행변경 후 항상 0번째 열부터 체크 필요
 
-			while (cy < d[0].length) {
-				Point px = new Point(ix, cy);
-				st.push(px);
-				count++;
-				break;
-			}
-
-			if (cy != d[0].length) {
-				break;
-			} else {
-				p = st.pop();
-				count--;
+			// 행 반복하며 퀸 배치가능여부 판단
+			while (ix < d.length) {
+				cy = NextMove(d, ix, cy); // 배치가능: 열 인덱스, 배치불가: d[0].length 반환
+				// 퀸 배치가능한 동안 반복?
+				while (cy < d[0].length) { // else문의 cy++ 때문에 필요
+					Point px = new Point(ix, cy);
+					st.push(px);
+					count++;
+					d[ix][cy] = 1;
+					break;
+				}
+				if (cy != d[0].length) {
+					break;
+				} else {
+					p = st.pop();
+					ix = p.getX();
+					cy = p.getY();
+					count--;
+					d[ix][cy] = 0;
+					cy++;
+				}
 			}
 		}
 	}
 
-	public static boolean checkRow(int[][] d, int crow) {
-		for (int i = 0; i < d[0].length; i++)
-			if (d[crow][i] == 1)
+	public static boolean checkRow(int[][] d, int cx, int cy) {
+		for (int i = 0; i < cy; i++)
+			if (d[cx][i] == 1)
 				return false;
 		return true;
 	}
 
-	public static boolean checkCol(int[][] d, int ccol) {
-		for (int i = 0; i < d.length; i++)
-			if (d[i][ccol] == 1)
+	public static boolean checkCol(int[][] d, int cx, int cy) {
+		for (int i = 0; i < cx; i++) {
+			if (d[i][cy] == 1)
 				return false;
+		}
 		return true;
 	}
 
 	public static boolean checkDiagSW(int[][] d, int cx, int cy) { // x++, y-- or x--, y++ where 0<= x,y <= 7
-		while (cx < d.length - 1 && cy > 0) {
-			cx++;
-			cy--;
-			if (d[cx][cy] == 1)
+		int x = cx, y = cy;
+		while (x < d.length - 1 && y > 0) {
+			x++;
+			y--;
+			if (d[x][y] == 1)
 				return false;
 		}
-		while (cx > 0 && cy < d[0].length - 1) {
-			cx--;
-			cy++;
-			if (d[cx][cy] == 1)
+		x = cx;
+		y = cy;
+		while (x > 0 && y < d[0].length - 1) {
+			x--;
+			y++;
+			if (d[x][y] == 1)
 				return false;
 		}
 		return true;
 	}
 
 	public static boolean checkDiagSE(int[][] d, int cx, int cy) {// x++, y++ or x--, y--
-		while (cx < d.length - 1 && cy < d[0].length - 1) {
-			cx++;
-			cy++;
-			if (d[cx][cy] == 1)
+		int x = cx, y = cy;
+		while (x < d.length - 1 && y < d[0].length - 1) {
+			x++;
+			y++;
+			if (d[x][y] == 1)
 				return false;
 		}
-		while (cx > 0 && cy > 0) {
-			cx--;
-			cy--;
-			if (d[cx][cy] == 1)
+		x = cx;
+		y = cy;
+		while (x > 0 && y > 0) {
+			x--;
+			y--;
+			if (d[x][y] == 1)
 				return false;
 		}
 		return true;
 	}
 
 	public static boolean CheckMove(int[][] d, int x, int y) {// (x,y)로 이동 가능한지를 check
-		if (checkRow(d, x) || checkCol(d, y) || checkDiagSW(d, x, y) || checkDiagSE(d, x, y)) {
-			return false;
+		if (checkRow(d, x, y) && checkCol(d, x, y) && checkDiagSW(d, x, y) && checkDiagSE(d, x, y)) {
+			return true;
 		}
-		return true;
+		return false;
 	}
 
 	public static int NextMove(int[][] d, int row, int col) {// 다음 row에 대하여 이동할 col을 조사
-		int nextCol = col + 1; // 다음 col로 넘어가기 위한 선언
-		while (nextCol <= d[0].length) 		// 인덱스 수만큼 반복
-			if (CheckMove(d, row, col))		// 현재 셀 체크가능여부 판단
-				return col;					// 체크할 수 있으면 col 반환
-		return -1;							// 체크할 수 있는 셀 없으면 -1 반환
+		int nextCol = col; // 다음 col로 넘어가기 위한 선언
+		while (nextCol < d[0].length) // 인덱스 수만큼 반복
+			if (CheckMove(d, row, nextCol)) // 현재 셀 체크가능여부 판단
+				return nextCol; // 체크할 수 있으면 col 반환
+			else
+				nextCol++;
+		return d[0].length; // 체크할 수 있는 셀 없으면 -1 반환
 	}
 
 	public static void main(String[] args) {
