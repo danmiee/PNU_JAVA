bde2241fa1b7792ec8a1ba9f848d2425ce9e9edc
danmiee <dan-mi@kakao.com>
2022-11-04 09:11:40 +0900

    MergeSort 기본틀

---
+++ DataStructure/src/ch06_sorting/date221104/MergeSort.java
@@ -0,0 +1,34 @@
+package ch06_sorting.date221104;
+
+public class MergeSort {
+
+	// recursive로 구현: stack을 이용한 backtracking 구현은 너무 힘들다 > recursive 알고리즘 구현 실습
+	
+	// --- 배열 요소 a[idx1]와 a[idx2]의 값을 교환 ---//
+	static void merge(int[] a, int lefta, int righta, int leftb, int rightb ) {
+
+	}
+
+	// --- 퀵 정렬(비재귀 버전)---//
+	static void MergeSort(int[] a, int left, int right) {
+	
+	}
+
+	public static void main(String[] args) {
+		int nx = 20;
+		int[] x = new int[20];
+		for (int ix = 0; ix < 20; ix++) {
+			double d = Math.random();
+			x[ix] = (int) (d * 50);
+		}
+		for (int i = 0; i < nx; i++)
+			System.out.print(" " + x[i]);
+		System.out.println();
+
+		MergeSort(x, 0, nx - 1); // 배열 x를 퀵정렬
+
+		System.out.println("오름차순으로 정렬했습니다.");
+		for (int i = 0; i < nx; i++)
+			System.out.print(" " + x[i]);
+	}
+}
