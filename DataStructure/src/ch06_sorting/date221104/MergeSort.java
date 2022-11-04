b18732271805301335503880d2d64244ff43dcf0
danmiee <dan-mi@kakao.com>
2022-11-04 18:57:53 +0900

    머지소트 관련 로직으로 주석 수정 및 초반 코드 일부 작성

--- DataStructure/src/ch06_sorting/date221104/MergeSort.java
+++ DataStructure/src/ch06_sorting/date221104/MergeSort.java
@@ -3,20 +3,38 @@ package ch06_sorting.date221104;
 public class MergeSort {
 
 	// recursive로 구현: stack을 이용한 backtracking 구현은 너무 힘들다 > recursive 알고리즘 구현 실습
-	
-	// --- 배열 요소 a[idx1]와 a[idx2]의 값을 교환 ---//
-	static void merge(int[] a, int lefta, int righta, int leftb, int rightb ) {
+	static Stack<Point> st = new Stack<Point>();
+	// --- 스택 활용하여 재귀적으로 병합 정렬 ---//
+	static void merge(int[] a, int left, int right) {
 
+		if (left < right) {
+			int center = (left + right) / 2;
+			Point pl = new Point(left, center);
+			Point pr = new Point(center+1, right);
+
+			// pl, pr에 대하여 재귀 적용
+			merge(a, pl.getX(), pl.getY());
+			merge(a, pr.getX(), pr.getY());
+			
+			// 배열요소 2개 이상이면 pr 스택에 담기
+			if(a.length>1)
+				st.push(pr);
+			else {
+				
+			}
+
+		}
 	}
 
-	// --- 퀵 정렬(비재귀 버전)---//
+	// --- 병합 정렬 ---//
 	static void MergeSort(int[] a, int left, int right) {
-	
+
 	}
 
 	public static void main(String[] args) {
 		int nx = 20;
 		int[] x = new int[20];
+
 		for (int ix = 0; ix < 20; ix++) {
 			double d = Math.random();
 			x[ix] = (int) (d * 50);
