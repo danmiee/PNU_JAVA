65fe2b69ba16ffaf555ba0d826173851ad0e3369
danmiee <dan-mi@kakao.com>
2022-11-08 20:23:33 +0900

    백준 2292번 클래스 & static 메소드 활용

---
+++ SelfStudy/src/baekjoon/step07/math/q02_Honeycomb3.java
@@ -0,0 +1,36 @@
+package baekjoon.step07.math;
+
+import java.util.Scanner;
+
+class MinStatic {
+	
+	public static int min(int num) {
+		int cnt = 1;
+		int rangeM = 2;
+		if(num == 1)
+			return 1;
+		else {
+			while(rangeM<=num) {
+				rangeM += 6*cnt;
+				cnt++;
+			}
+		}
+		return cnt;
+	}	
+}
+
+public class q02_Honeycomb3 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("숫자를 입력하면 지나는 방의 최소 개수를 구합니다.(종료: 0)");
+		int N;
+		while((N=sc.nextInt())!=0) {
+		
+			System.out.println(MinStatic.min(N));
+		}
+		sc.close();
+	}
+}
