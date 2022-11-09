86bc62fdf667ff039d2c6125c1aedd7405efb411
danmiee <dan-mi@kakao.com>
2022-11-09 10:22:49 +0900

    머지정렬 완성

--- DataStructure/src/ch06_sorting/date221107/MergeSortTest.java
+++ DataStructure/src/ch06_sorting/date221107/MergeSortTest.java
@@ -1,14 +1,65 @@
 package ch06_sorting.date221107;
 
-public class MergeSortTest {
-	// --- 배열 요소 a[idx1]와 a[idx2]의 값을 교환 ---//
-	static void merge(int[] a, int lefta, int righta, int leftb, int rightb ) {
+import java.util.Arrays;
 
+public class MergeSortTest {
+
+	static void merge(int[] a, int lefta, int righta, int leftb, int rightb ) {
+		int[] arr1 = Arrays.copyOfRange(a, lefta, righta);
+        int[] arr2 = Arrays.copyOfRange(a, leftb, rightb);
+        int[] arr3 = new int[a.length];
+        
+        int i = 0, j = 0, k = 0;
+		while (i < arr1.length && j < arr2.length) {
+			if (arr1[i] < arr2[j]) {
+				arr3[k] = arr1[i];
+				i++;
+				k++;
+			} else if (arr1[i] > arr2[j]) {
+				arr3[k] = arr2[j];
+				j++;
+				k++;
+			} else {
+				arr3[k] = arr1[i];
+				i++;
+				k++;
+				j++;
+			}
+		}
+		while (i < arr1.length) {
+			arr3[k] = arr1[i];
+			i++;
+			k++;
+		}
+
+		while (j < arr2.length) {
+			arr3[k] = arr2[j];
+			j++;
+			k++;
+		}
 	}
 
-	// --- 퀵 정렬(비재귀 버전)---//
 	static void MergeSort(int[] a, int left, int right) {
-	
+		
+		int center = (left + right)/2;
+
+		if(left<right) {
+			
+			MergeSort(a, left, center);
+			MergeSort(a, center+1, right);	
+		
+		for(int i=0; i<a.length-1; i++) {
+			int j=i+1;
+			if(a[i]>a[j]) {
+				int temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+			}
+		}
+		
+		merge(a, left, center, center+1, right);
+		
+		}
 	}
 
 	public static void main(String[] args) {
