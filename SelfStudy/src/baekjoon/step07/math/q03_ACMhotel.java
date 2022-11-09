1bfea5b9266e4420fc83b53268fda171688d2332
danmiee <dan-mi@kakao.com>
2022-11-09 20:32:39 +0900

    의사코드 작성

---
+++ SelfStudy/src/baekjoon/step07/math/q03_ACMhotel.java
@@ -0,0 +1,30 @@
+package baekjoon.step07.math;
+
+import java.util.Scanner;
+
+public class q03_ACMhotel {
+
+	public static void assign(int H, int W, int N) {
+		// 아래층 우선 배정
+		// 층+호 값 구하기
+		// 층 계산
+		// 호 계산
+		// N 관련 등차수열 값에 따라 층 1부터 반복
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("입력할 테스트 데이터 개수: ");
+		int T = sc.nextInt();
+		
+		System.out.print("호텔의 층 수, 방 수, 몇번째 손님: ");
+		int H = sc.nextInt();
+		int W = sc.nextInt();
+		int N = sc.nextInt();
+		
+		
+		
+		sc.close();
+	}
+}
