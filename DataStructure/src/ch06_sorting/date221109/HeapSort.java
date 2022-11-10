fda6f0a4f10197343ef1b6ee42ab17ea94140f0c
danmiee <dan-mi@kakao.com>
2022-11-10 20:51:05 +0900

    힙소트 C++ to JAVA 구현 완성
    - Element 클래스
    	- key 접근제한자 private으로 변경
    	- 게터/세터 생성
    	- toString 오버라이딩
    - MaxPQ 인터페이스
    	- Insert 메소드 매개변수 final 삭제
    	- DeleteMax 메소드 매개변수 삭제
    - MaxHeap 클래스
    	- 기본 생성자 삭제
    	- 매개변수 보유 생성자 재정의
    	- size 메소드 추가(Delete 시 currentMax 출력용)
    	- Insert 메소드 작성
    	- DeleteMax 메소드 작성
    	- display 메소드 수정
    - HeapSort 클래스
    	- main 메소드 재정리

--- DataStructure/src/ch06_sorting/date221109/HeapSort.java
+++ DataStructure/src/ch06_sorting/date221109/HeapSort.java
@@ -3,92 +3,126 @@ package ch06_sorting.date221109;
 import java.util.Scanner;
 
 class Element {
-	public int key;
+	private int key;
+
 	public Element() {
-		
+
 	}
+
 	public Element(int key) {
 		this.key = key;
+	}
+
+	public int getKey() {
+		return key;
+	}
+
+	public void setKey(int key) {
+		this.key = key;
+	}
+
+	@Override
+	public String toString() {
+		return "" + key + "";
 	}
 }
 
 interface MaxPQ {
-	public void Insert(final Element x);
-	public Element DeleteMax(Element x);
+	public void Insert(Element x);
+
+	public Element DeleteMax();
 }
 
 class MaxHeap implements MaxPQ {
 	private Element[] heap;
 	private int n; // current size of MaxHeap
 	private int MaxSize; // Maximum allowable size of MaxHeap
-	
-	public MaxHeap() {
-	}
-	
+
 	public MaxHeap(int sz) {
-		sz = MaxSize;
+		MaxSize = sz;
+		n = 0;
 		heap = new Element[MaxSize + 1]; // Don't want to use heap[0]
+	}
+
+	public int size() {
+		return n;
 	}
 	
 	public void HeapEmpty() {
 		System.out.println("Heap Empty");
 	}
-	
+
 	public void HeapFull() {
 		System.out.print("Heap Full");
 	}
-	
+
 	@Override
-	public void Insert(final Element x) {
-		int i;
+	public void Insert(Element x) {
 		if (n == MaxSize) {
 			HeapFull();
 			return;
 		}
+		// 힙 크기 증가
 		n++;
-		for (i = n; i > 0;) {
+		// 정렬
+		for (int i = n; i >= 1; i /= 2) {
+			// x를 리프에 넣어주기
+			heap[i] = x;
+			// 루트까지 돌고 반복문 중단
 			if (i == 1)
 				break; // at root
-			if (x.key <= heap[i / 2].key)
+			// x의 키값이 부모의 키값보다 작으면 반복문 중단
+			else if (x.getKey() < heap[i / 2].getKey())
 				break;
+			// 반복문 중단되지 않았으면 자리 바꾸기
 			// move from parent to i
 			heap[i] = heap[i / 2];
-			i /= 2;
+			heap[i / 2] = x;
 		}
-		heap[i] = x;
 	}
-	
+
 	@Override
-	public Element DeleteMax(Element x) {
+	public Element DeleteMax() {
+
 		int i, j;
+		Element temp = heap[1];
+
 		if (n == 0) {
 			HeapEmpty();
 			return null;
 		}
-		x = heap[1];
+
+		// 값이 1개이면 해당값 리턴하고 포인터 -
+		if (n == 1) {
+			n--;
+			return temp;
+		}
+
+		// 아직 리턴 안됐으면 리턴할 k 고정
 		Element k = heap[n];
 		n--;
-		
-		for (i = 1, j = 2; j <= n;) {
-			if (j < n)
-				if (heap[j].key < heap[j + 1].key)
-					j++;
+
+		// 정렬하기(i:부모인덱스, j:자식인덱스)
+		for (i = 1, j = 2; j <= n; j *= 2) {
+			// 자식인덱스가 힙의 크기보다 작을 때 더 큰 자식의 인덱스 선택
 			// j points to the larger child
-			if (k.key >= heap[j].key)
-				break;
-			heap[i] = heap[j];
-			i = j;
-			j *= 2;
+			if (j < n && heap[j].getKey() < heap[j + 1].getKey())
+				j++;
+			// 부모가 자식보다 크거나 같으면 패스
+			if (heap[i].getKey() >= heap[j].getKey())
+				continue;
+			// 패스되지 않았으면 자리바꾸기
+			temp = heap[j];
+			heap[j] = heap[i];
+			heap[i] = temp;
 		}
-		heap[i] = k;
-		return x;
+		return k;
 	}
-	
+
 	public void display() {
-		int i;
-		System.out.println("MaxHeap:: (i, heap[i].key): ");
-		for (i = 1; i <= n; i++)
-			System.out.println("(" + i + ", " + heap[i].key + ") ");
+		System.out.printf("MaxHeap::\n(i, heap[i].getKey()):\n");
+		for (int i = 1; i <= n; i++)
+			System.out.println("(" + i + ", " + heap[i].getKey() + ") ");
 	}
 }
 
@@ -96,42 +130,39 @@ public class HeapSort {
 	// simple queue에 대한 성능 비교 실험, 난수를 사용하여 시간 측정
 	// MinPQ를 구현
 
-	final static int HeapSize = 1000;
+	static int HeapSize = 1000;
 
 	enum Boolean {
 		FALSE, TRUE
 	};
 
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		int select = 0;
-		Element data = null;
-		
+		int size = 0;
 		MaxHeap heap = new MaxHeap(20);
-		Element ele = null;
-		
 		Element deletedEle = null;
 
 		do {
-			System.out.println("Max Tree. Select: 1 insert, 2 display, 3 delete, >=5 exit");
+			System.out.println("Max Tree. Select: (1) insert, (2) display, (3) delete, (5) exit");
 			select = sc.nextInt();
 			switch (select) {
 			case 1:
 				System.out.print("input value: ");
+				int ipv = sc.nextInt();
+				Element data = new Element(ipv);
 				heap.Insert(data);
-				heap.display();
 				break;
 			case 2:
 				heap.display();
 				break;
 			case 3:
-				deletedEle = heap.DeleteMax(ele);
+				deletedEle = heap.DeleteMax();
+				size = heap.size();
 				if (deletedEle != null)
-					System.out.print("deleted element: " + deletedEle);
-				System.out.print("current max heap: ");
-				heap.display();
+					System.out.println("deleted element: " + deletedEle);
+				System.out.println("current max heap: " + size);
 				break;
 			case 5:
 				break;
