e4346e91c0b010c559bf7f9f0124253d31b6e2d9
danmiee <dan-mi@kakao.com>
2022-11-11 19:46:35 +0900

    백준 10250번 ACM호텔 - 클래스 활용 / static 메소드

---
+++ SelfStudy/src/baekjoon/step07/math/q03_ACMhotel2.java
@@ -0,0 +1,40 @@
+package baekjoon.step07.math;
+
+import java.util.Scanner;
+
+class Assignment {
+	static int assign(int H, int W, int N) {
+		// 층 수: y, 호 수: x
+		int y = 0, x = 0;
+		
+		if (N % H == 0) {
+			y = H*100;
+			x = N / H;
+		} else {
+			y = (N % H) * 100;
+			x = N / H + 1;
+		}
+		return x+y;
+	}
+}
+public class q03_ACMhotel2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("입력할 테스트 데이터 개수: ");
+		int T = sc.nextInt();
+		int n = 0;
+		while (n < T) {
+			System.out.print("호텔의 층 수, 방 수, 몇번째 손님: ");
+
+			int H = sc.nextInt();
+			int W = sc.nextInt();
+			int N = sc.nextInt();
+			
+			System.out.println(Assignment.assign(H, W, N));
+			
+			n++;
+		}
+		sc.close();
+	}
+}
