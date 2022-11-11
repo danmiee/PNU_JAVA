c998980486f40d8b85953b6ad8274baaf3c40371
danmiee <dan-mi@kakao.com>
2022-11-11 19:46:05 +0900

    백준 10250번 ACM호텔

--- SelfStudy/src/baekjoon/step07/math/q03_ACMhotel.java
+++ SelfStudy/src/baekjoon/step07/math/q03_ACMhotel.java
@@ -4,27 +4,35 @@ import java.util.Scanner;
 
 public class q03_ACMhotel {
 
-	public static void assign(int H, int W, int N) {
-		// 아래층 우선 배정
-		// 층+호 값 구하기
-		// 층 계산
-		// 호 계산
-		// N 관련 등차수열 값에 따라 층 1부터 반복
+	static int assign(int H, int W, int N) {
+		// 층 수: y, 호 수: x
+		int y = 0, x = 0;
+
+		if (N % H == 0) {
+			y = H*100;
+			x = N / H;
+		} else {
+			y = (N % H) * 100;
+			x = N / H + 1;
+		}
+		return x+y;
 	}
-	
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
 		System.out.print("입력할 테스트 데이터 개수: ");
 		int T = sc.nextInt();
-		
-		System.out.print("호텔의 층 수, 방 수, 몇번째 손님: ");
-		int H = sc.nextInt();
-		int W = sc.nextInt();
-		int N = sc.nextInt();
-		
-		
-		
+		int n = 0;
+		while (n < T) {
+			System.out.print("호텔의 층 수, 방 수, 몇번째 손님: ");
+
+			int H = sc.nextInt();
+			int W = sc.nextInt();
+			int N = sc.nextInt();
+
+			System.out.println(assign(H, W, N));
+			n++;
+		}
 		sc.close();
 	}
 }
