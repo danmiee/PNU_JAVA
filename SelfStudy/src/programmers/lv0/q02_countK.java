f4ff10623ac4c335fcaafe02a453980436879282
danmiee <dan-mi@kakao.com>
2023-01-11 20:48:17 +0900

    k 갯수 세기

--- SelfStudy/src/programmers/lv0/q02_countK.java
+++ SelfStudy/src/programmers/lv0/q02_countK.java
@@ -13,47 +13,37 @@ i부터 j까지 k가 몇 번 등장하는지 return 하도록 solution 함수를 완성해주세요.
  */
 public class q02_countK {
 
-//    public static int solution(int i, int j, int k) {
-//        int answer = 0;
-//        String str = "";
-//        for (int x=i; x<=j; i++) {
-//        	str += x;
-//        }
-//        
-//        return answer;
-//    }
-    
-    @SuppressWarnings("unused")
-	public static void main(String[] args) {
-    	int i=1;
-    	int j=13;
-    	int k=1;
-    	
+    public static int solution(int i, int j, int k) {
     	String str = "";
         for (int x=i; x<=j; x++) {
         	str += x;
         }
         
         int cnt=0;
-        for (int x=0; x<str.length(); x++) {
-        	if( str.charAt(x)==k)
-        		cnt+=1;
+        for (int x=0; x < str.length(); x++) {
+        	if(((int) str.charAt(x))-48 == k) {
+        		cnt += 1;
+        	}
         }
+        return cnt;
+    }
+    
+    @SuppressWarnings("unused")
+	public static void main(String[] args) {
         
-        System.out.println(cnt);
-//		 Scanner sc = new Scanner(System.in);
-//		 int n;
-//		 System.out.println("확인할 데이터 수를 입력하세요");
-//		 while((n=sc.nextInt())!=0) {
-//			 System.out.println("시작값을 입력하세요");
-//			 int i = sc.nextInt();
-//			 System.out.println("종료값을 입력하세요");
-//			 int j = sc.nextInt();
-//			 System.out.println("등장횟수를 확인할 수를 입력하세요");
-//			 int k = sc.nextInt();
-//			 
-//			 System.out.println(solution(i, j, k));
-//		 }
-//		 sc.close();
+		 Scanner sc = new Scanner(System.in);
+		 int n;
+		 System.out.println("확인할 데이터 수를 입력하세요");
+		 while((n=sc.nextInt())!=0) {
+			 System.out.println("시작값을 입력하세요");
+			 int i = sc.nextInt();
+			 System.out.println("종료값을 입력하세요");
+			 int j = sc.nextInt();
+			 System.out.println("등장횟수를 확인할 수를 입력하세요");
+			 int k = sc.nextInt();
+			 
+			 System.out.println(solution(i, j, k));
+		 }
+		 sc.close();
 	}
 }
